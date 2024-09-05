c2fb0593b2a84691b66344799e752abc944caded
psc3245 <psc3.omaha@gmail.com>
2024-09-05 18:10:52 -0500

    Version 1.0 + Cross Origin Support

--- kitchenInv_Rest/src/main/java/com/psc/kitchenInv/controllers/FruitController.java
+++ kitchenInv_Rest/src/main/java/com/psc/kitchenInv/controllers/FruitController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 
 @RestController
+@CrossOrigin
 public class FruitController {
 
     private FruitService fruitService;
--- kitchenInv_Rest/src/main/java/com/psc/kitchenInv/controllers/VegetableController.java
+++ kitchenInv_Rest/src/main/java/com/psc/kitchenInv/controllers/VegetableController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 public class VegetableController {
 
     private VegetableService vegetableService;
